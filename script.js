4483258e4d8037d609a68367e3e991032c33abcf
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-25 15:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function shortcut(s1, s2) {
   // your code here
-	if (!s1 || !s2) {
-    return '';
-  } else {
-    return s1.charAt(0).toUpperCase() + s2.charAt(0).toUpperCase();
-  }
+
+	if (str1.isEmpty() || str2.isEmpty()) {
+        return "";
+    } else {
+        // Get the initial letters of the strings
+        char initial1 = str1.charAt(0);
+        char initial2 = str2.charAt(0);
+        
+        // Return the initials as a string
+        return String.valueOf(initial1) + String.valueOf(initial2);
+    }
+	
 }
 
 // Do not change the code below.
